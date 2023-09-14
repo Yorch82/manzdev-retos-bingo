87496f7aac1194220e41f6b4fd51c59ccb12f143
Yorch82 <jcampo1982@outlook.com>
2023-09-14 14:26:37 +0200

    logic finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const startButton = document.getElementById("button");
+
+const bolasSacadas = [];
+
 function creaCarton() {
   const numerosAleatorios = [];
   for (let i = 0; i < 15; i++) {
@@ -8,6 +12,20 @@ function creaCarton() {
   }
   return numerosAleatorios;
 }
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Intercambia los elementos
+  }
+}
+
+const bombo = [];
+for (let i = 1; i <= 90; i++) {
+  bombo.push(i);
+}
+
+shuffleArray(bombo);
 
 const cartonJugador = creaCarton();
 const cartonCPU = creaCarton();
@@ -23,5 +41,49 @@ function pintaCarton(selector, array) {
   }
 }
 
+function pintaBolasSacadas(selector, elemento) {
+  const parent = document.querySelector(`.${selector}`);
+  const divcarton = document.createElement("div");
+  divcarton.className = "numero";
+  divcarton.textContent = elemento;
+  parent.appendChild(divcarton);
+}
+
 pintaCarton("player", cartonJugador);
 pintaCarton("cpu", cartonCPU);
+
+startButton.addEventListener("click", () => {
+  sacarBola();
+});
+
+function sacarBola() {
+  const indiceBola = Math.floor(Math.random() * bombo.length);
+  const bola = bombo[indiceBola];
+  bombo.splice(indiceBola, 1);
+  bolasSacadas.push(bola);
+  pintaBolasSacadas("bolas", bola);
+  const divbola = document.querySelector("#button");
+  divbola.textContent = bola;
+  const numero = document.querySelectorAll(`.n${bola}`);
+  for (let i = 0; i < numero.length; i++) {
+    numero[i].classList.add("tachado");
+  }
+  const totalJugador = document.querySelectorAll(".player .tachado").length;
+  const totalCpu = document.querySelectorAll(".cpu .tachado").length;
+  if (totalJugador === 15) {
+    startButton.classList.add("hide");
+    const parent = document.getElementById("bola");
+    const divGanador = document.createElement("div");
+    divGanador.className = "winner";
+    divGanador.innerText = "PLAYER WINS!!";
+    parent.appendChild(divGanador);
+  }
+  if (totalCpu === 15) {
+    startButton.classList.add("hide");
+    const parent = document.getElementById("bola");
+    const divGanador = document.createElement("div");
+    divGanador.className = "winner";
+    divbola.innerText = "CPU WINS!!";
+    parent.appendChild(divGanador);
+  }
+}
